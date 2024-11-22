335ffbc7176dec216eca75e7b6237bc0a5d1b0fe
xiekang <zhangxiekang@gmail.com>
2024-11-22 19:49:44 +0100

    only support java 11 and add core function mapelements

---
+++ java_beam/src/main/java/de/xiekang/beam/core/Task2.java
@@ -0,0 +1,48 @@
+package de.xiekang.beam.core;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+public class Task2 {
+    private final static Logger LOGGER = LogManager.getLogger(Task2.class.getName());
+
+    static class LogOutput<T> extends DoFn<T, T> {
+        private String prefix;
+
+        public LogOutput(String prefix) {
+            this.prefix = prefix;
+        }
+
+        public LogOutput() {
+            this.prefix = "Processing Element";
+        }
+
+        @ProcessElement
+        public void processElement(@Element T element) {
+            LOGGER.info(prefix + ": " + element);
+        }
+    }
+
+    public static void main(String... args) {
+        Configurator.setRootLevel(Level.INFO);
+
+        PipelineOptions pipelineOptions = PipelineOptionsFactory.fromArgs(args).create();
+        Pipeline pipeline = Pipeline.create(pipelineOptions);
+
+        pipeline.apply("Create Input", Create.of(10, 20, 30, 40, 50))
+                .apply("Map Element", MapElements.into(TypeDescriptors.integers()).via(number -> number * 5))
+                .apply("Log Output", ParDo.of(new LogOutput<>("Multiplied Element")));
+
+        pipeline.run().waitUntilFinish();
+    }
+}
